d13c961e3a96b7b0ba2855ddf2338e0893183ef6
digitalpartisan <digital.partisan.86@gmail.com>
2018-04-14 15:28:19 -0700

    Fixing a bug in the up/down bat files

--- up-packed.bat
+++ up-packed.bat
@@ -1,1 +1,1 @@
-mklink /h "K:\Steam\steamapps\common\Fallout 4\Data\CheatBunkerWastelandWorkshop - Main.ba2" "K:\FO4 Mods\CheatBunkerVaultTec\Data\CheatBunkerWastelandWorkshop - Main.ba2"
+mklink /h "K:\Steam\steamapps\common\Fallout 4\Data\CheatBunkerWastelandWorkshop - Main.ba2" "K:\FO4 Mods\CheatBunkerWastelandWorkshop\Data\CheatBunkerWastelandWorkshop - Main.ba2"
